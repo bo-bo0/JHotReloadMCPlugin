f05f7db061614e3acf8cd9c470907b1eb96136aa
bo bo <pierlucaperla@gmail.com>
2026-08-07 01:16:13 +0200

    add JHotReload setup so that is now possible to use the library with Hot Variables declared in custom code and by editing the JSON files directly

--- src/main/java/net/jhotreloadplugin/JHotReloadPlugin.java
+++ src/main/java/net/jhotreloadplugin/JHotReloadPlugin.java
@@ -1,26 +1,18 @@
 package net.jhotreloadplugin;
 
+import net.jhotreloadplugin.installation.JHotReloadWorkspaceInstaller;
 import net.mcreator.plugin.JavaPlugin;
 import net.mcreator.plugin.Plugin;
 import net.mcreator.plugin.events.workspace.MCreatorLoadedEvent;
-import net.mcreator.ui.action.BasicAction;
-import net.mcreator.ui.init.L10N;
-import net.mcreator.ui.init.UIRES;
-import net.mcreator.workspace.Workspace;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.swing.JMenu;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
-import java.awt.Dimension;
+
 
 public class JHotReloadPlugin extends JavaPlugin
 {
-    private static final Logger LOG =
-            LogManager.getLogger("JHotReload Plugin");
+    private static final Logger LOG = LogManager.getLogger("JHotReload plugin");
 
     public JHotReloadPlugin(Plugin plugin)
     {
@@ -29,107 +21,12 @@ public class JHotReloadPlugin extends JavaPlugin
         addListener(MCreatorLoadedEvent.class, event ->
                 SwingUtilities.invokeLater(() ->
                 {
-                    var workspaceReportAction = new BasicAction(
-                            event.getMCreator().getActionRegistry(),
-                            L10N.t("plugin.jhotreloadplugin.menu.button"),
-                            actionEvent -> showWorkspaceReport(
-                                    event.getMCreator().getWorkspace(),
-                                    event.getMCreator()
-                            )
-                    );
-
-                    workspaceReportAction.setIcon(UIRES.get("16px.play"));
-
-                    var menu = new JMenu(
-                            L10N.t("plugin.jhotreloadplugin.menu.main")
-                    );
-
-                    menu.add(workspaceReportAction);
-
-                    event.getMCreator()
-                            .getMainMenuBar()
-                            .add(menu);
-
-                    event.getMCreator()
-                            .getToolBar()
-                            .addToRightToolbar(workspaceReportAction);
+                    var workspace = event.getMCreator().getWorkspace();
+                    var installedPath = JHotReloadWorkspaceInstaller.install(workspace);
+                    LOG.info("JHotReload installed in workspace: {}", installedPath);
                 })
         );
 
-        LOG.info("JHotReload Plugin was loaded");
-    }
-
-    private static void showWorkspaceReport(
-            Workspace workspace,
-            java.awt.Component parent
-    )
-    {
-        var report = new StringBuilder();
-
-        report.append("MCreator workspace report")
-                .append(System.lineSeparator())
-                .append("=========================")
-                .append(System.lineSeparator())
-                .append(System.lineSeparator());
-
-        report.append("MCreator version: ")
-                .append(workspace.getMCreatorVersion())
-                .append(System.lineSeparator());
-
-        report.append("Mod elements: ")
-                .append(workspace.getModElements().size())
-                .append(System.lineSeparator());
-
-        report.append("Variables: ")
-                .append(workspace.getVariableElements().size())
-                .append(System.lineSeparator());
-
-        report.append("Sounds: ")
-                .append(workspace.getSoundElements().size())
-                .append(System.lineSeparator());
-
-        report.append("Tags: ")
-                .append(workspace.getTagElements().size())
-                .append(System.lineSeparator());
-
-        report.append(System.lineSeparator())
-                .append("Mod elements")
-                .append(System.lineSeparator())
-                .append("------------")
-                .append(System.lineSeparator());
-
-        if (workspace.getModElements().isEmpty())
-        {
-            report.append("No mod elements found.")
-                    .append(System.lineSeparator());
-        }
-        else
-        {
-            for (var modElement : workspace.getModElements())
-            {
-                report.append("- ")
-                        .append(modElement.getName())
-                        .append(" [")
-                        .append(modElement.getTypeString())
-                        .append(']')
-                        .append(System.lineSeparator());
-            }
-        }
-
-        var textArea = new JTextArea(report.toString());
-
-        textArea.setEditable(false);
-        textArea.setCaretPosition(0);
-
-        var scrollPane = new JScrollPane(textArea);
-
-        scrollPane.setPreferredSize(new Dimension(600, 400));
-
-        JOptionPane.showMessageDialog(
-                parent,
-                scrollPane,
-                "Workspace Report",
-                JOptionPane.INFORMATION_MESSAGE
-        );
+        LOG.info("JHotReload plugin was loaded");
     }
 }
---
+++ src/main/java/net/jhotreloadplugin/installation/JHotReloadWorkspaceInstaller.java
@@ -0,0 +1,52 @@
+package net.jhotreloadplugin.installation;
+
+import net.mcreator.workspace.Workspace;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public final class JHotReloadWorkspaceInstaller
+{
+    private static final String RESOURCE_PATH = "/bundled-libs/JHotReload.jar";
+    private static final String LIBRARY_FILE_NAME = "JHotReload.jar";
+
+    private JHotReloadWorkspaceInstaller() {}
+
+    public static Path install(Workspace workspace)
+    {
+        var targetDirectory = workspace
+                .getWorkspaceFolder()
+                .toPath()
+                .resolve("lib");
+
+        var targetPath = targetDirectory.resolve(LIBRARY_FILE_NAME);
+
+        try
+        {
+            Files.createDirectories(targetDirectory);
+
+            if (Files.exists(targetPath))
+            { return targetPath; }
+
+            try (var inputStream = JHotReloadWorkspaceInstaller.class.getResourceAsStream(RESOURCE_PATH))
+            {
+                if (inputStream == null)
+                { throw new IllegalStateException("Bundled JHotReload library not found"); }
+
+                Files.copy
+                (
+                    inputStream,
+                    targetPath,
+                    StandardCopyOption.REPLACE_EXISTING
+                );
+            }
+
+            return targetPath;
+        }
+        catch (IOException exception)
+        { throw new UncheckedIOException("Failed to install JHotReload into workspace", exception); }
+    }
+}
